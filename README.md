a82f7424e115787bc54974f42361007a2c8e1c37
Andrew Hunn <ahunn@umich.edu>
2021-12-01 01:17:14 -0500

    initial csnap bundle for testing

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Arbitrary style transfer in TensorFlow.js
 
+### Bundle for CSnap
+
+```bash
+yarn run prep
+```
+
+Then, you can run
+
+```bash
+yarn run start
+```
+
+To build, run
+
+```bash
+yarn run csnapdeploy
+```
+
+Afterwards, copy images, and the four saved_model directories. Copy the entire visualizer directory, then
+add to the middleware folder in csnap_pro. Add the bundle js file before the csdt src files in snap.html.
+
+### Old README
+
 This repository contains an implementation of arbitrary style transfer running fully
 inside the browser using TensorFlow.js.
 
@@ -20,10 +43,10 @@ Blog post with more details: https://magenta.tensorflow.org/blog/2018/12/20/styl
 ### What is this?
 
 This is an implementation of an arbitrary style transfer algorithm
-running purely in the browser using TensorFlow.js. As with all neural 
-style transfer algorithms, a neural network attempts to "draw" one 
-picture, the Content (usually a photograph), in the style of another, 
-the Style (usually a painting). 
+running purely in the browser using TensorFlow.js. As with all neural
+style transfer algorithms, a neural network attempts to "draw" one
+picture, the Content (usually a photograph), in the style of another,
+the Style (usually a painting).
 
 Although [other browser implementations](https://github.com/reiinakano/fast-style-transfer-deeplearnjs)
 of style transfer exist,
@@ -32,40 +55,40 @@ the requirement that a separate neural network must be trained for each
 style image.
 
 Arbitrary style transfer works around this limitation by using a
-separate *style network* that learns to break down *any* image into 
-a 100-dimensional vector representing its style. This style vector is 
-then fed into another network, the *transformer network*, along
+separate _style network_ that learns to break down _any_ image into
+a 100-dimensional vector representing its style. This style vector is
+then fed into another network, the _transformer network_, along
 with the content image, to produce the final stylized image.
 
-I have written a [blog post](https://magenta.tensorflow.org/blog/2018/12/20/style-transfer-js/) 
+I have written a [blog post](https://magenta.tensorflow.org/blog/2018/12/20/style-transfer-js/)
 explaining this project in more detail.
 
 ### Is my data safe? Can you see my pictures?
 
 Your data and pictures here never leave your computer! In fact,
-this is one of the main advantages of running neural networks 
-in your browser. Instead of sending us your data, we send *you* 
-both the model *and* the code to run the model. These are then 
+this is one of the main advantages of running neural networks
+in your browser. Instead of sending us your data, we send _you_
+both the model _and_ the code to run the model. These are then
 run by your browser.
 
 ### What are all these different models?
 
-The original paper uses an Inception-v3 model 
-as the style network, which takes up ~36.3MB 
+The original paper uses an Inception-v3 model
+as the style network, which takes up ~36.3MB
 when ported to the browser as a FrozenModel.
 
 In order to make this model smaller, a MobileNet-v2 was
-used to distill the knowledge from the pretrained Inception-v3 
+used to distill the knowledge from the pretrained Inception-v3
 style network. This resulted in a size reduction of just under 4x,
 from ~36.3MB to ~9.6MB, at the expense of some quality.
 
-For the transformer network, the original paper uses 
+For the transformer network, the original paper uses
 a model using plain convolution layers. When ported to
 the browser, this model takes up 7.9MB and is responsible
 for the majority of the calculations during stylization.
 
 In order to make the transformer model more efficient, most of the
-plain convolution layers were replaced with depthwise separable 
+plain convolution layers were replaced with depthwise separable
 convolutions. This reduced the model size to 2.4MB, while
 drastically improving the speed of stylization.
 
@@ -76,20 +99,20 @@ transformer network.
 ### How big are the models I'm downloading?
 
 The distilled style network is ~9.6MB, while the separable convolution
-transformer network is ~2.4MB, for a total of ~12MB. 
-Since these models work for any style, you only 
+transformer network is ~2.4MB, for a total of ~12MB.
+Since these models work for any style, you only
 have to download them once!
 
 ### How does style combination work?
 
-Since each style can be mapped to a 100-dimensional 
+Since each style can be mapped to a 100-dimensional
 style vector by the style network,
 we simply take a weighted average of the two to get
 a new style vector for the transformer network.
 
 This is also how we are able to control the strength
-of stylization. We take a weighted average of the style 
-vectors of *both* content and style images and use 
+of stylization. We take a weighted average of the style
+vectors of _both_ content and style images and use
 it as input to the transformer network.
 
 ## Running locally for development
@@ -110,21 +133,20 @@ yarn run start
 
 You can then browse to `localhost:9966` to view the application.
 
-
 ## Credits
 
 This demo could not have been done without the following:
 
-* Authors of the [arbitrary style transfer](https://arxiv.org/abs/1705.06830) paper.
-* The Magenta repository for [arbitrary style transfer](https://github.com/tensorflow/magenta/tree/master/magenta/models/arbitrary_image_stylization).
-* Authors of [the MobileNet-v2 paper](https://arxiv.org/abs/1801.04381).
-* Authors of the paper describing [neural network knowledge distillation](https://arxiv.org/abs/1503.02531).
-* The [TensorFlow.js library](https://js.tensorflow.org).
-* [Google Colaboratory](https://colab.research.google.com/), with which I was able 
-to do all necessary training using a free(!) GPU.
+- Authors of the [arbitrary style transfer](https://arxiv.org/abs/1705.06830) paper.
+- The Magenta repository for [arbitrary style transfer](https://github.com/tensorflow/magenta/tree/master/magenta/models/arbitrary_image_stylization).
+- Authors of [the MobileNet-v2 paper](https://arxiv.org/abs/1801.04381).
+- Authors of the paper describing [neural network knowledge distillation](https://arxiv.org/abs/1503.02531).
+- The [TensorFlow.js library](https://js.tensorflow.org).
+- [Google Colaboratory](https://colab.research.google.com/), with which I was able
+  to do all necessary training using a free(!) GPU.
 
-As a final note, I'd love to hear from people interested 
-in making a suite of tools for artistically manipulating images, kind of like 
+As a final note, I'd love to hear from people interested
+in making a suite of tools for artistically manipulating images, kind of like
 [Magenta Studio](https://magenta.tensorflow.org/studio)
-but for images. Please reach out if you're planning to build/are 
+but for images. Please reach out if you're planning to build/are
 building one out!
---
+++ core.js
@@ -0,0 +1,322 @@
+/* eslint-disable */
+import "babel-polyfill";
+import * as tf from "@tensorflow/tfjs";
+tf.ENV.set("WEBGL_PACK", false); // This needs to be done otherwise things run very slow v1.0.4
+import links from "./links";
+
+let styleOptions = ["mobilenet", "inception"];
+let transformerOptions = ["separable", "original"];
+
+window.testA = {
+  contentImage: "images/chicago.jpg",
+  sourceImage: "images/statue_of_liberty.jpg",
+  styleModel: "inception",
+  transformModel: "original",
+  styleRatio: 1,
+};
+
+window.testB = {
+  contentImage: "images/towers.jpg",
+  sourceImage: "images/red_circles.jpg",
+  styleModel: "inception",
+  transformModel: "original",
+  styleRatio: 0.23,
+};
+
+class Core {
+  constructor() {
+    Promise.all([
+      this.loadMobileNetStyleModel(),
+      this.loadSeparableTransformerModel(),
+    ]).then(([styleNet, transformNet]) => {
+      console.log("Loaded styleNet");
+      this.styleNet = styleNet;
+      this.transformNet = transformNet;
+      // this.enableStylizeButtons();
+    });
+  }
+
+  generateStylizedImage(options) {
+    let generic = {
+      contentImage: "images/beach.jpg",
+      sourceImage: "images/statue_of_liberty.jpg",
+      styleModel: "mobilenet",
+      transformModel: "separable",
+      styleRatio: 0.5,
+    };
+
+    if (options) {
+      Object.assign(generic, options);
+    }
+
+    this.contentImg = document.createElement("IMG");
+    this.contentImg.style.height = "400px";
+    this.contentImg.style.width = "100%";
+    this.contentImg.src = "src/middleware/visualizer/" + generic.contentImage;
+
+    this.styleImg = document.createElement("IMG");
+    this.styleImg.style.height = "400px";
+    this.styleImg.style.width = "100%";
+    this.styleImg.src = generic.sourceImage;
+
+    this.styleRatio = generic.styleRatio;
+    this.stylized = document.createElement("CANVAS");
+
+    Promise.all([this.loadStyleModel(), this.loadTransformModel()]).then(
+      ([styleNet, transformNet]) => {
+        console.log("Loaded styleNet");
+        this.styleNet = styleNet;
+        this.transformNet = transformNet;
+
+        console.log(styleNet, transformNet);
+        this.startStyling().finally(() => {
+          var a = document.createElement("a");
+          a.setAttribute("download", "output.jpeg");
+          a.setAttribute(
+            "href",
+            this.stylized
+              .toDataURL("image/jpeg", 1.0)
+              .replace("image/jpeg", "image/octet-stream")
+          );
+
+          // a.download = "output.png";
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+        });
+      }
+    );
+  }
+
+  async loadStyleModel(style) {
+    return style == "inception"
+      ? this.loadInceptionStyleModel()
+      : this.loadMobileNetStyleModel();
+  }
+
+  async loadTransformModel(transform) {
+    return transform == "original"
+      ? this.loadOriginalTransformerModel()
+      : this.loadSeparableTransformerModel();
+  }
+
+  async loadMobileNetStyleModel() {
+    if (!this.mobileStyleNet) {
+      this.mobileStyleNet = await tf.loadGraphModel(
+        "src/middleware/visualizer/saved_model_style_js/model.json"
+      );
+    }
+
+    return this.mobileStyleNet;
+  }
+
+  async loadInceptionStyleModel() {
+    if (!this.inceptionStyleNet) {
+      this.inceptionStyleNet = await tf.loadGraphModel(
+        "src/middleware/visualizer/saved_model_style_inception_js/model.json"
+      );
+    }
+
+    return this.inceptionStyleNet;
+  }
+
+  async loadOriginalTransformerModel() {
+    if (!this.originalTransformNet) {
+      this.originalTransformNet = await tf.loadGraphModel(
+        "src/middleware/visualizer/saved_model_transformer_js/model.json"
+      );
+    }
+
+    return this.originalTransformNet;
+  }
+
+  async loadSeparableTransformerModel() {
+    if (!this.separableTransformNet) {
+      this.separableTransformNet = await tf.loadGraphModel(
+        "src/middleware/visualizer/saved_model_transformer_separable_js/model.json"
+      );
+    }
+
+    return this.separableTransformNet;
+  }
+
+  async startStyling() {
+    await tf.nextFrame();
+    // this.styleButton.textContent = "Generating 100D style representation";
+    await tf.nextFrame();
+    let bottleneck = await tf.tidy(() => {
+      return this.styleNet.predict(
+        tf.browser
+          .fromPixels(this.styleImg)
+          .toFloat()
+          .div(tf.scalar(255))
+          .expandDims()
+      );
+    });
+    if (this.styleRatio !== 1.0) {
+      // this.styleButton.textContent =
+      //   "Generating 100D identity style representation";
+      await tf.nextFrame();
+      const identityBottleneck = await tf.tidy(() => {
+        return this.styleNet.predict(
+          tf.browser
+            .fromPixels(this.contentImg)
+            .toFloat()
+            .div(tf.scalar(255))
+            .expandDims()
+        );
+      });
+      const styleBottleneck = bottleneck;
+      bottleneck = await tf.tidy(() => {
+        const styleBottleneckScaled = styleBottleneck.mul(
+          tf.scalar(this.styleRatio)
+        );
+        const identityBottleneckScaled = identityBottleneck.mul(
+          tf.scalar(1.0 - this.styleRatio)
+        );
+        return styleBottleneckScaled.addStrict(identityBottleneckScaled);
+      });
+      styleBottleneck.dispose();
+      identityBottleneck.dispose();
+    }
+    // this.styleButton.textContent = "Stylizing image...";
+    await tf.nextFrame();
+    const stylized = await tf.tidy(() => {
+      return this.transformNet
+        .predict([
+          tf.browser
+            .fromPixels(this.contentImg)
+            .toFloat()
+            .div(tf.scalar(255))
+            .expandDims(),
+          bottleneck,
+        ])
+        .squeeze();
+    });
+    await tf.browser.toPixels(stylized, this.stylized);
+    bottleneck.dispose(); // Might wanna keep this around
+    stylized.dispose();
+  }
+
+  async startCombining() {
+    await tf.nextFrame();
+    // this.combineButton.textContent =
+    //   "Generating 100D style representation of image 1";
+    await tf.nextFrame();
+    const bottleneck1 = await tf.tidy(() => {
+      return this.styleNet.predict(
+        tf.browser
+          .fromPixels(this.combStyleImg1)
+          .toFloat()
+          .div(tf.scalar(255))
+          .expandDims()
+      );
+    });
+
+    // this.combineButton.textContent =
+    //   "Generating 100D style representation of image 2";
+    await tf.nextFrame();
+    const bottleneck2 = await tf.tidy(() => {
+      return this.styleNet.predict(
+        tf.browser
+          .fromPixels(this.combStyleImg2)
+          .toFloat()
+          .div(tf.scalar(255))
+          .expandDims()
+      );
+    });
+
+    // this.combineButton.textContent = "Stylizing image...";
+    await tf.nextFrame();
+    const combinedBottleneck = await tf.tidy(() => {
+      const scaledBottleneck1 = bottleneck1.mul(
+        tf.scalar(1 - this.combStyleRatio)
+      );
+      const scaledBottleneck2 = bottleneck2.mul(tf.scalar(this.combStyleRatio));
+      return scaledBottleneck1.addStrict(scaledBottleneck2);
+    });
+
+    const stylized = await tf.tidy(() => {
+      return this.transformNet
+        .predict([
+          tf.browser
+            .fromPixels(this.combContentImg)
+            .toFloat()
+            .div(tf.scalar(255))
+            .expandDims(),
+          combinedBottleneck,
+        ])
+        .squeeze();
+    });
+    await tf.browser.toPixels(stylized, this.combStylized);
+    bottleneck1.dispose(); // Might wanna keep this around
+    bottleneck2.dispose();
+    combinedBottleneck.dispose();
+    stylized.dispose();
+  }
+
+  async benchmark() {
+    const x = tf.randomNormal([1, 256, 256, 3]);
+    const bottleneck = tf.randomNormal([1, 1, 1, 100]);
+
+    let styleNet = await this.loadInceptionStyleModel();
+    let time = await this.benchmarkStyle(x, styleNet);
+    styleNet.dispose();
+
+    styleNet = await this.loadMobileNetStyleModel();
+    time = await this.benchmarkStyle(x, styleNet);
+    styleNet.dispose();
+
+    let transformNet = await this.loadOriginalTransformerModel();
+    time = await this.benchmarkTransform(x, bottleneck, transformNet);
+    transformNet.dispose();
+
+    transformNet = await this.loadSeparableTransformerModel();
+    time = await this.benchmarkTransform(x, bottleneck, transformNet);
+    transformNet.dispose();
+
+    x.dispose();
+    bottleneck.dispose();
+  }
+
+  async benchmarkStyle(x, styleNet) {
+    const profile = await tf.profile(() => {
+      tf.tidy(() => {
+        const dummyOut = styleNet.predict(x);
+        dummyOut.print();
+      });
+    });
+    console.log(profile);
+    const time = await tf.time(() => {
+      tf.tidy(() => {
+        for (let i = 0; i < 10; i++) {
+          const y = styleNet.predict(x);
+          y.print();
+        }
+      });
+    });
+    console.log(time);
+  }
+
+  async benchmarkTransform(x, bottleneck, transformNet) {
+    const profile = await tf.profile(() => {
+      tf.tidy(() => {
+        const dummyOut = transformNet.predict([x, bottleneck]);
+        dummyOut.print();
+      });
+    });
+    console.log(profile);
+    const time = await tf.time(() => {
+      tf.tidy(() => {
+        for (let i = 0; i < 10; i++) {
+          const y = transformNet.predict([x, bottleneck]);
+          y.print();
+        }
+      });
+    });
+    console.log(time);
+  }
+}
+
+window.application = new Core();
+console.log(window.application);
